9adac52fce17b7b21ce00188d536643b9f2dad36
Urmi-Jana <janaurmi6@gmail.com>
2023-08-18 17:20:47 +0530

    add eleven labs tts api

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const express = require ('express')
 const ejs = require('ejs')
 const port = process.env.PORT || 3000;
+const axios = require('axios')
+const voiceID = "21m00Tcm4TlvDq8ikWAM" //has to be changed
+const API = process.env.API
 
 const app = express()
 app.use(express.urlencoded({extended: true}))
@@ -9,15 +12,44 @@ app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-var tasks = ['drink water', 'ask X to hang out']
+const tasks = ['drink water', 'ask X to hang out']
+const text = tasks[0]
+const voice = '21m00Tcm4TlvDq8ikWAM'
 
 app.get('/', function(req, res){
     res.render('home', {tasks: tasks});
     // console.log(req.body.task);
 })
 
-app.post('/', function(req, res){
+app.post('/', async function(req, res){
     console.log(req.body.tasks);
+    const voiceSettings = {
+        stability: 0.75,
+        similarity_boost: 0.75,
+    }
+    
+    try{
+        const response = await axios.post(
+            `https://api.elevenlabs.io/v1/text-to-speech/${voice}`,
+            {
+                text: text,
+                voice_settings: voiceSettings,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    accept: 'audio/mpeg',
+                    'xi-api-key': API,
+                },
+                responseType: 'arraybuffer',
+            }
+        )
+        console.log(response.data);
+    }catch(error){
+        console.log(error.response);
+    }
+
+    
     res.redirect('/')
 })
 
